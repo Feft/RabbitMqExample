8c55f4eda18130d0795d80e65627ba006d21f9c8
Feft <piotrpikula@gmail.com>
2017-11-21 19:01:41 +0100

    clean Symfony3 application

---
+++ AppBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Simple Symfony Bundle to learn how to use RabbitMq with Symfony3
+ */
+class AppBundle extends Bundle
+{
+}
