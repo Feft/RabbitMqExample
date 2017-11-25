6758bcee86b776e7b6a44020c2402650b00e528b
Feft <piotrpikula@gmail.com>
2017-11-25 15:54:05 +0100

    code refactoring

--- Services/ExampleConsumer.php
+++ Services/ExampleConsumer.php
@@ -7,6 +7,11 @@ use Doctrine\ORM\EntityManager;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 
+/**
+ * Simple rabbitMq consumer
+ * This class is initialize by a service.
+ * To execute this code run the command:  php bin/console bbitmq:consumer <<queue name>>
+ */
 class ExampleConsumer implements ConsumerInterface
 {
     /**
@@ -22,6 +27,8 @@ class ExampleConsumer implements ConsumerInterface
     }
 
     /**
+     * Receives data from the queue and executes the task.
+     *
      * @param AMQPMessage $msg The message
      *
      * @return mixed false to reject and requeue, any other value to acknowledge
@@ -38,11 +45,23 @@ class ExampleConsumer implements ConsumerInterface
         $this->em->persist($data);
         $this->em->flush();
 
-        echo 'received message ' . $message['id'] . ', created at ' . $message['datetime'] . ",\r\n" .
-            "highest common factor of " . $message["firstValue"] . " and " . $message["secondValue"] . " is " . $hcf . "\r\n";
+        echo $this->getInfo($message);
 //        sleep(0.1);
 
         return true;
+    }
+
+    /**
+     * Preparing the message.
+     *
+     * @param array $message Data to show
+     *
+     * @return string
+     */
+    private function getInfo(array $message)
+    {
+        return 'received message ' . $message['id'] . ', created at ' . $message['datetime'] . ",\r\n" .
+            "highest common factor of " . $message["firstValue"] . " and " . $message["secondValue"] . " is " . $message["hcf"] . "\r\n";
     }
 
     /**
